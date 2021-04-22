224fa8ef27a52f62e9d0ef0b05f08fbe5837aa75
jasperdg <jasperdegooijer@gmail.com>
2021-04-23 00:53:08 +0200

    removed println from test

--- amm/tests/it/fee_tests.rs
+++ amm/tests/it/fee_tests.rs
@@ -238,7 +238,6 @@ fn fee_invalid_market_lp_fee_test() {
         deposit = STORAGE_AMOUNT
     );
 
-    println!("{:?}", trader_claim_res);
     assert!(trader_claim_res.is_ok());
 
     // Get updated balances
