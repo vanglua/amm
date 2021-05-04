59947231c9ace22cea31d7c8fd9593a0ce27f246
Jameson Hodge <contact@jamesonhodge.com>
2021-05-04 16:08:01 -0400

    create resolution_time property; create basic unit test

--- amm/src/market.rs
+++ amm/src/market.rs
@@ -1,8 +1,11 @@
 use crate::*;
+use std::convert::TryInto;
+use collateral_whitelist::Token;
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Market {
-    pub end_time: u64, // Time when trading is halted and the market can be resoluted
+    pub end_time: u64, // Time when trading is halted
+    pub resolution_time: u64, // Time when the market can be resoluted
     pub pool: Pool, // Implementation that manages the liquidity pool and swap
     pub payout_numerator: Option<Vec<U128>>, // Optional Vector that dictates how payout is done. Each payout numerator index corresponds to an outcome and shares the denomination of te collateral token for this market.
     pub finalized: bool, // If true the market has an outcome, if false the market it still undecided.
@@ -150,7 +153,8 @@ impl AMMContract {
      * @param outcomes the number of possible outcomes for the market
      * @param outcome_tags is a list of outcomes where the index is the `outcome_id`
      * @param categories is a list of categories to filter the market by
-     * @param end_time when the trading should stop and the market can be resolved
+     * @param end_time when the trading should stop
+     * @param resolution_time when the market can be resolved
      * @param collateral_token_id the `account_id` of the whitelisted token that is used as collateral for trading
      * @param swap_fee the fee that's taken from every swap and paid out to LPs
      * @param is_scalar if the market is a scalar market (range)
@@ -165,18 +169,21 @@ impl AMMContract {
         outcome_tags: Vec<String>,
         categories: Vec<String>,
         end_time: U64,
+        resolution_time: U64,
         collateral_token_id: AccountId,
         swap_fee: U128,
         is_scalar: Option<bool>,
     ) -> U64 {
         self.assert_unpaused();
         let end_time: u64 = end_time.into();
+        let resolution_time: u64 = resolution_time.into();
         let swap_fee: u128 = swap_fee.into();
         let market_id = self.markets.len();
         let token_decimals = self.collateral_whitelist.0.get(&collateral_token_id);
         assert!(token_decimals.is_some(), "ERR_INVALID_COLLATERAL");
         assert!(outcome_tags.len() as u16 == outcomes, "ERR_INVALID_TAG_LENGTH");
         assert!(end_time > ns_to_ms(env::block_timestamp()), "ERR_INVALID_END_TIME");
+        assert!(resolution_time >= end_time, "ERR_INVALID_RESOLUTION_TIME");
         let initial_storage = env::storage_usage();
 
         let pool = pool_factory::new_pool(
@@ -191,6 +198,7 @@ impl AMMContract {
 
         let market = Market {
             end_time,
+            resolution_time,
             pool,
             payout_numerator: None,
             finalized: false
@@ -225,7 +233,7 @@ impl AMMContract {
         let collateral_out: u128 = collateral_out.into();
         let mut market = self.markets.get(market_id.into()).expect("ERR_NO_MARKET");
         assert!(!market.finalized, "ERR_FINALIZED_MARKET");
-        assert!(market.end_time > ns_to_ms(env::block_timestamp()), "ERR_MARKET_ENDED");
+        assert!(market.resolution_time > ns_to_ms(env::block_timestamp()), "ERR_MARKET_ENDED");
         let escrowed = market.pool.sell(
             &env::predecessor_account_id(),
             collateral_out,
@@ -474,4 +482,81 @@ impl AMMContract {
 
         self.markets.replace(args.market_id.into(), &market);
     }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod market_basic_tests {
+    use near_sdk::{ MockedBlockchain };
+    use near_sdk::{ testing_env, VMContext };
+    use super::*;
+
+    fn alice() -> AccountId {
+        "alice.near".to_string()
+    }
+
+    fn bob() -> AccountId {
+        "bob.near".to_string()
+    }
+
+    fn empty_string() -> String {
+        "".to_string()
+    }
+
+    fn empty_string_vec(len: u16) -> Vec<String> {
+        let mut tags: Vec<String> = vec![];
+        for i in 0..len {
+            tags.push(empty_string());
+        }
+    tags
+    }
+
+    fn get_context(predecessor_account_id: AccountId, timestamp: u64) -> VMContext {
+        VMContext {
+            current_account_id: alice(),
+            signer_account_id: bob(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id,
+            input: vec![],
+            block_index: 0,
+            block_timestamp: timestamp,
+            account_balance: 1000 * 10u128.pow(24),
+            account_locked_balance: 0,
+            storage_usage: 10u64.pow(6),
+            attached_deposit: 15600000000000000000000,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+            epoch_height: 0,
+        }
+    }
+
+    #[test]
+    fn basic_create_market() {
+        testing_env!(get_context(alice(), 0));
+
+        let mut contract = AMMContract::init(
+            bob().try_into().unwrap(),
+            vec![collateral_whitelist::Token{account_id: "token".to_string(), decimals: 24}]
+        );
+
+        contract.create_market(
+            empty_string(), // market description
+            empty_string(), // extra info
+            2, // outcomes
+            empty_string_vec(2), // outcome tags
+            empty_string_vec(2), // categories
+            1609951265967.into(), // end_time
+            1619882574000.into(), // resolution_time (~1 day after end_time)
+            "token".to_string(), // collateral_token_id
+            (10_u128.pow(24) / 50).into(), // swap fee, 2%
+            None // is_scalar
+        );
+        //testing_env!(get_context(alice(), 0));
+    }
+
+    // #[should_panic(expected = "ERR_MARKET_ENDED")]
+    // fn withdraw_liquidity_after_resolution_time() {}
+
 }
