4dc40c5c8d171e4829a1a193a7879099510e03e3
Jameson Hodge <contact@jamesonhodge.com>
2021-05-04 16:28:40 -0400

    build.sh: make ./res dir if doesn't exist

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+
+if [ ! -d ./res ]; then
+    mkdir ./res
+fi
+
 cp ./target/wasm32-unknown-unknown/release/amm.wasm ./res
 cp ./target/wasm32-unknown-unknown/release/dao.wasm ./res
 cp ./target/wasm32-unknown-unknown/release/oracle.wasm ./res
